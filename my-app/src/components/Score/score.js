c8cb508393bc2c00956e9c199dac7eeb3e72c594
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-02-05 22:48:15 -0800

    final commit

--- my-app/src/components/Score/score.js
+++ my-app/src/components/Score/score.js
@@ -9,11 +9,11 @@ const Score = props => (
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
           aria-expanded="false">
         </button>
-        <a class="navbar-brand"><strong>Clicky Game</strong></a>
+        <a class="navbar-brand" className="display"><strong>Clicky Game</strong></a>
       </div>
       
       <div class="navbar-header navbar-right">
-        <a class="navbar-brand"><strong>Score:{props.addPoint}</strong></a>
+        <a class="navbar-brand" className="display"><strong>Score:{props.addPoint}</strong></a>
         
       </div>
     </div>
